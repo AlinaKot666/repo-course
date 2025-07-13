4acf3f6c4d1fbee517e3e56bfb93ec298436b3ec
AlinaKot666 <alinakotliarenko666@gmail.com>
2025-07-13 11:24:24 +0200

    Add chalk usage to index.js

--- nodeBasic/index.js
+++ nodeBasic/index.js
@@ -1,2 +1,5 @@
-const chlak = require ('chalk');
-console.log (chalk.blue('Hola'));
+const chalk = require('chalk');
+
+console.log(chalk.blue('Hola'));
+console.log(chalk.green('Привет!'));
+console.log(chalk.red('Это красный текст'));
