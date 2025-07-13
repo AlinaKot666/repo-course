3a6df1bc91cc738e81c88b55db8cb827a5f8e890
AlinaKot666 <alinakotliarenko666@gmail.com>
2025-07-13 16:52:10 +0200

    Add all JS basic tasks

---
+++ JSBasics/tasc4.js
@@ -0,0 +1,16 @@
+// 4.1: Площадь круга
+const circleRadius = 5;
+const circleArea = Math.PI * Math.pow(circleRadius, 2);
+console.log("Площадь круга:", circleArea.toFixed(2));
+
+// Завдання 4.2: Площадь прямоугольника
+const length = 10;
+const width = 4;
+const rectangleArea = length * width;
+console.log("Площа прямоугольника:", rectangleArea.toFixed(2));
+
+// Завдання 4.3: Обьем цилиндра
+const cylinderRadius = 3;
+const height = 7;
+const cylinderVolume = Math.PI * Math.pow(cylinderRadius, 2) * height;
+console.log("Обьем цилиндра:", cylinderVolume.toFixed(2));
