178fdb68849447e1cd4dcb421568d0b53fd13188
AlinaKot666 <alinakotliarenko666@gmail.com>
2026-03-12 18:29:56 +0100

    Refactor task1.js to integrate Book and EBook classes
    Refactored task1.js to use Book and EBook classes, added instances, and implemented functionality to find the oldest book and create an EBook from a Book.

--- lesson10/task1.js
+++ lesson10/task1.js
@@ -1,57 +1,29 @@
-lesson10/Book.js
-class Book {
-  constructor(title, author, year) {
-    this._title = title;
-    this._author = author;
-    this._year = year;
-  }
+const Book = require("./Book");
+const EBook = require("./EBook");
 
-  // getters
-  get title() {
-    return this._title;
-  }
+// обычные книги
+const book1 = new Book("The Lord of the Rings", "J.R.R. Tolkien", 1954);
+const book2 = new Book("Pride and Prejudice", "Jane Austen", 1813);
+const book3 = new Book("The Little Prince", "Antoine de Saint-Exupéry", 1943);
 
-  get author() {
-    return this._author;
-  }
+// электронная книга
+const ebook1 = new EBook("Atomic Habits", "James Clear", 2018, "EPUB");
 
-  get year() {
-    return this._year;
-  }
+// вывод информации
+book1.printInfo();
+book2.printInfo();
+book3.printInfo();
+ebook1.printInfo();
 
-  // setters with validation
-  set title(value) {
-    if (typeof value !== "string" || value.length === 0) {
-      throw new Error("Title must be a non-empty string");
-    }
-    this._title = value;
-  }
+// массив книг (и Book, и EBook)
+const books = [book1, book2, book3, ebook1];
 
-  set author(value) {
-    if (typeof value !== "string" || value.length === 0) {
-      throw new Error("Author must be a non-empty string");
-    }
-    this._author = value;
-  }
+// найти самую старую книгу
+const oldestBook = Book.getOldestBook(books);
+console.log("\nOldest book in the list:");
+oldestBook.printInfo();
 
-  set year(value) {
-    if (typeof value !== "number" || value <= 0) {
-      throw new Error("Year must be a positive number");
-    }
-    this._year = value;
-  }
-
-  // method to print info
-  printInfo() {
-    console.log(`Book: ${this.title}, Author: ${this.author}, Year: ${this.year}`);
-  }
-
-  // static method to find oldest book
-  static getOldestBook(books) {
-    return books.reduce((oldest, book) => {
-      return book.year < oldest.year ? book : oldest;
-    });
-  }
-}
-
-module.exports = Book;
+// создать EBook из обычной книги
+const ebookFromBook = EBook.fromBook(book2, "PDF");
+console.log("\nEBook created from Book:");
+ebookFromBook.printInfo();
